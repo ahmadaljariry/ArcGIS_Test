11da69a7ec58747859019e5459539b59ff0390f1
ahmadaljariry <ahmad_aljariry@hotmail.com>
2014-01-04 09:12:45 +0400

    pic

--- Office_Location/gen/com/example/office_location/R.java
+++ Office_Location/gen/com/example/office_location/R.java
@@ -23,6 +23,7 @@ public final class R {
     public static final class drawable {
         public static final int dialog_full_holo_dark=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int searchicon=0x7f020002;
     }
     public static final class id {
         public static final int CompanyName=0x7f080004;
